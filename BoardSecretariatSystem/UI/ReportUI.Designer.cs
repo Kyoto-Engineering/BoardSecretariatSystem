502824d8a3e3d3e7a09283497e3f93651537c80d
Iqbal Hossain <Iqbal@kyoto.local>
2017-07-21 03:48:46 +0600

    Report ui modified

--- BoardSecretariatSystem/UI/ReportUI.Designer.cs
+++ BoardSecretariatSystem/UI/ReportUI.Designer.cs
@@ -38,7 +38,14 @@
             this.meetingMinutesButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // attendenceSlipButton
@@ -137,11 +144,79 @@
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 82);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Form-IX";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(316, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 82);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Form-X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(545, 488);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 82);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Form-VI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(804, 400);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 82);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Company Name";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(16, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(185, 21);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(804, 488);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(223, 82);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Form-XII";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // ReportUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 493);
+            this.ClientSize = new System.Drawing.Size(1039, 605);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.meetingMinutesButton);
             this.Controls.Add(this.participantAttendenceReportButton);
@@ -155,6 +230,7 @@
             this.Text = "ReportUI";
             this.Load += new System.EventHandler(this.ReportUI_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -170,5 +246,11 @@
         private System.Windows.Forms.Button meetingMinutesButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
     }
 }
--- BoardSecretariatSystem/UI/ReportUI.cs
+++ BoardSecretariatSystem/UI/ReportUI.cs
@@ -22,7 +22,7 @@ namespace BoardSecretariatSystem.UI
         private SqlDataReader rdr;
         private ConnectionString cs = new ConnectionString();
         public int x;
-        //public int y;
+        public string y;
         public ReportUI()
         {
             InitializeComponent();
@@ -369,6 +369,7 @@ namespace BoardSecretariatSystem.UI
         private void ReportUI_Load(object sender, EventArgs e)
         {
             meetingidLoad();
+            CompanyNameLoad();
         }
 
         private void meetingidLoad()
@@ -395,6 +396,32 @@ namespace BoardSecretariatSystem.UI
             }
         }
 
+        private void CompanyNameLoad()
+        {
+            try
+            {
+                con = new SqlConnection(cs.DBConn);
+                con.Open();
+                string ct = "SELECT  CompanyName FROM Company order by CompanyName asc";
+                cmd = new SqlCommand(ct);
+                cmd.Connection = con;
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    comboBox2.Items.Add(rdr[0]);  //comboBox2.Items.Add(rdr.GetValue(0).ToString());
+                }
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             try
@@ -419,6 +446,218 @@ namespace BoardSecretariatSystem.UI
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //creating an object of ParameterField class
+           // ParameterField paramField = new ParameterField();
+
+            //creating an object of ParameterFields class
+           // ParameterFields paramFields = new ParameterFields();
+
+            //creating an object of ParameterDiscreteValue class
+            //ParameterDiscreteValue paramDiscreteValue = new ParameterDiscreteValue();
+
+            //set the parameter field name
+            //paramField.Name = "meetingid";
+
+            //set the parameter value
+            //paramDiscreteValue.Value = x;
+
+            //add the parameter value in the ParameterField object
+            //paramField.CurrentValues.Add(paramDiscreteValue);
+
+            //add the parameter in the ParameterFields object
+           // paramFields.Add(paramField);
+
+            //set the parameterfield information in the crystal report
+
+
+
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BoardSecretariatDBKD";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+
+            Form_IX cr = new Form_IX();
+            
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            
+            this.Visible = false;
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BoardSecretariatDBKD";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+
+            Form_X cr = new Form_X();
+
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+
+            this.Visible = false;
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BoardSecretariatDBKD";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+
+            Form_VI cr = new Form_VI();
+
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+
+            this.Visible = false;
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(cs.DBConn);
+                con.Open();
+                string ct = "select CompanyName FROM Company where CompanyName='" + comboBox2.Text + "'";
+                cmd = new SqlCommand(ct);
+                cmd.Connection = con;
+                rdr = cmd.ExecuteReader();
+
+                if (rdr.Read())
+                {
+
+                    y = (rdr.GetString(0));
+
+                }
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //creating an object of ParameterField class
+            ParameterField paramField = new ParameterField();
+
+            //creating an object of ParameterFields class
+            ParameterFields paramFields = new ParameterFields();
+
+            //creating an object of ParameterDiscreteValue class
+            ParameterDiscreteValue paramDiscreteValue = new ParameterDiscreteValue();
+
+            //set the parameter field name
+            paramField.Name = "Company_Name";
+
+            //set the parameter value
+            paramDiscreteValue.Value = y;
+
+            //add the parameter value in the ParameterField object
+            paramField.CurrentValues.Add(paramDiscreteValue);
+
+            //add the parameter in the ParameterFields object
+            paramFields.Add(paramField);
+
+            //set the parameterfield information in the crystal report
+
+
+
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BoardSecretariatDBKD";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+
+            Form_XII cr = new Form_XII();
+            
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            
+            f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            
+            this.Visible = false;
+            f2.ShowDialog();
+            this.Visible = true;
         }
        
     }
